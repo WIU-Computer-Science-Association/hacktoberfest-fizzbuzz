d94ef64def2fbe76088a8ba6ce6a16cd1dac90c9
Evan Colwell <eccolwell99@gmail.com>
2019-10-18 22:32:36 -0500

    Comment update

--- evancolwell.rs
+++ evancolwell.rs
@@ -1,14 +1,16 @@
+//Fizzbuzz program in rust
 fn main(){
-    let mut x = 1;
+    //Rust requires fn main(){
+    let mut x = 1; //You need to make variables mutable (mut) to change them
     while x < 100 {
-        if x % 3 == 0 && x % 5 != 0{
-            println!("Fizz");
+        if x % 3 == 0 && x % 5 != 0{ //bool structure similar to java
+            println!("Fizz"); //Don't forget the ! in the println
         } else if x % 3 != 0 && x % 5 == 0 {
             println!("Buzz");
         } else if x % 3 == 0 && x % 5 == 0 {
             println!("FizzBuzz");
         } else {
-            println!("{}", x);
+            println!("{}", x); //println!() structures the string " ...{}..." and the variable is added in the {}
         }
         x += 1;
     }
